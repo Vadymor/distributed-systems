2e5eb217a4f5e0c75b01d560328c1bb4a480aa6d
alexzarembovskiy <64923641+alexzarembovskiy@users.noreply.github.com>
2022-10-07 20:30:57 +0200

    Logging was added

--- master_test.py
+++ master_test.py
@@ -1,15 +1,18 @@
 import requests
 import json
+import logging as lg
+from datetime import datetime
+
+lg.basicConfig(level=lg.DEBUG)
+lg.debug("The debug of the Master scripting logic has begun")
 
 payload = {
     "value": "message_4"
 }
 
 
-response = requests.post(url="http://0.0.0.0:8000/add-message/", data=json.dumps(payload))
+response = requests.post(url="http://127.0.0.1:8000/add-message/", data=json.dumps(payload))
 # response = requests.get(url="http://0.0.0.0:8000/get-messages/")
 # response = requests.get(url="http://0.0.0.0:8002/get-messages-secondary/")
 
-
-print(response.status_code)
-print(response.text)
+lg.debug(f"Response status code from the Master is: {response.status_code} at {datetime.now()}")
